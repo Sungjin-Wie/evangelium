6c25b35505da5ec7360d09148369ee51045352b9
Sungjin-Wie <tmdwls1203@gmail.com>
2021-08-11 15:46:42 +0900

    [Mod] fixed src/App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          유앙겔리온 테스트 홈페이지입니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
