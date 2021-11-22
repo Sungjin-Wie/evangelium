ad61a610270da5d6b04e156584d61138bbb74c3b
Sungjin-Wie <tmdwls1203@gmail.com>
2021-11-22 11:02:25 +0900

    [MOD] pickaxe icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
+import pickaxe from './pickaxe.png';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pickaxe} className="App-logo" alt="logo" />
         <p>
           유앙겔리온 홈페이지에 오신 것을 환영합니다.
+          홈페이지 공사중입니다!
         </p>
       </header>
     </div>
